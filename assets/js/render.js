9c1c81efb4c76c54107cb723860c6a40f9237fea
jaakofalltrade <jaako.andes@bicol-u.edu.ph>
2019-06-15 03:14:40 +0800

    added simple pagination design

--- assets/js/render.js
+++ assets/js/render.js
@@ -48,6 +48,52 @@ const findMatch = (word) => {
     });
 }
 
+const showPage = url => {
+    API.get(url)
+    .then(y => {
+        let dataBox = [];
+        y.results.map(result => { 
+            dataJSON.map(res => {
+                if(res.name == result.name) {
+                    dataBox.push(`
+                    <div class="item">
+                        <div class="card">
+                            <div class="face front">
+                                <div class="overlay">
+                                    <h1>${result.name}</h1>
+                                </div>
+                                <img src="${res.image}" alt="">
+                            </div>
+                            <div class="face back">
+                                <div class="back-info">
+                                    <div class="back-header"><p>Character Information</p></div>
+                                    <div class="back-body">
+                                        <p>Name:          ${result.name}</p>
+                                        <p>Gender:        ${result.gender}</p>
+                                        <p>Birthyear:     ${result.birth_year}</p>
+                                        <p>Eye Color:     ${result.eye_color}</p>
+                                        <p>Skin Color:    ${result.skin_color}</p>
+                                        <p>Height:        ${result.height}</p>
+                                        <p>Mass:          ${result.mass}</p>
+                                        <p>Species:       ${res.species}</p>
+                                        <p>Born Location: ${res.bornLocation}</p>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    `);
+                }
+            });
+        });
+        $('.body-container').html(`<div class="content-container">${dataBox.join('')}</div>
+        <div class="pagination"><button></button></div>
+        `);
+    });
+}
+
+showPage(swURL);
+
 $(document).ready(() => {
     $('.search-input').keyup(() => {
         let word = $('.search-input').val();
@@ -57,19 +103,3 @@ $(document).ready(() => {
         }
     });
 });
-
-/*if(y.name == x.name) {
-    x.map(result => {
-        dataBox.push(`
-        <div class="item">
-            <div class="card">
-                <div class="face front">
-                    <img src="${y}" alt="">
-                </div>
-                <div class="face back"><h2>${e.name}</h2></div>
-            </div>
-        </div>
-        `);
-    });
-    $('.content-container').html(res);
-}*/
