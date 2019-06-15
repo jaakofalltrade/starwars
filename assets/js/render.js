67349e0c62aa42609ff0e55ccbffd01c4f6b32dc
jaakofalltrade <jaako.andes@bicol-u.edu.ph>
2019-06-15 14:50:32 +0800

    Added pagination javascript, working on design of the buttons

--- assets/js/render.js
+++ assets/js/render.js
@@ -1,16 +1,16 @@
 console.log(dataJSON);
 const findMatch = (word) => {
     API.get(`${swURL}?search=${word}`)
-    .then(x => {
-        return x.results;
-    })
-    .then(y => {
-        let dataBox = [];
-        if((y != undefined) && (y != '')) {
-            y.map(result => { 
-                dataJSON.map(res => {
-                    if(res.name == result.name) {
-                        dataBox.push(`
+        .then(x => {
+            return x.results;
+        })
+        .then(y => {
+            let dataBox = [];
+            if ((y != undefined) && (y != '')) {
+                y.map(result => {
+                    dataJSON.map(res => {
+                        if (res.name == result.name) {
+                            dataBox.push(`
                         <div class="item">
                             <div class="card">
                                 <div class="face front">
@@ -38,24 +38,43 @@ const findMatch = (word) => {
                             </div>
                         </div>
                         `);
-                    }
+                        }
+                    });
                 });
-            });
-            $('.body-container').html(`<div class="content-container">${dataBox.join('')}</div>`);
-        } else {
-            $('.body-container').html(`<div class="cannot-find"><h1>Find what you are looking for, I cannot.</h1></div>`);
-        }
-    });
+                $('.body-container').html(`<div class="content-container">${dataBox.join('')}</div>`);
+            } else {
+                $('.body-container').html(`<div class="cannot-find"><h1>Find what you are looking for, I cannot.</h1></div>`);
+            }
+        });
 }
 
 const showPage = url => {
+    $('.body-container').html(`<div class="cannot-find"><img src="assets/images/loader.gif" alt=""></div>`);
+    if (url == 'null' || url == undefined) {
+        url = swURL;
+    } else {
+        $('.body-container').html(`<div class="cannot-find"><img src="assets/images/loader.gif" alt=""></div>`);
+    }
     API.get(url)
-    .then(y => {
-        let dataBox = [];
-        y.results.map(result => { 
-            dataJSON.map(res => {
-                if(res.name == result.name) {
-                    dataBox.push(`
+        .then(y => {
+            let prevButton;
+            let nextButton;
+            if (y.previous == null) {
+                prevButton = `<button disabled data-link="${y.previous}" title="Previous" class="page-btn disabled-btn"><i class="fa fa-lg fa-chevron-left"></i></button>`;
+            } else {
+                prevButton = `<button onclick="showPage('${y.previous}');" data-link="${y.previous}" title="Previous" class="page-btn"><i class="fa fa-lg fa-chevron-left"></i></button>`;
+            }
+
+            if (y.next == null) {
+                nextButton = `<button disabled data-link="${y.next}" title="Next" class="page-btn disabled-btn"><i class="fa fa-lg fa-chevron-right"></i></button>`;
+            } else {
+                nextButton = `<button onclick="showPage('${y.next}');" data-link="${y.next}" title="Next" class="page-btn"><i class="fa fa-lg fa-chevron-right"></i></button>`;
+            }
+            let dataBox = [];
+            y.results.map(result => {
+                dataJSON.map(res => {
+                    if (res.name == result.name) {
+                        dataBox.push(`
                     <div class="item">
                         <div class="card">
                             <div class="face front">
@@ -83,23 +102,31 @@ const showPage = url => {
                         </div>
                     </div>
                     `);
-                }
+                    }
+                });
             });
+            $('.body-container').html(`
+                <div class="content-container">${dataBox.join('')}</div>
+                <div class="pagination">
+                    <div class="pagination-cont">
+                        ${prevButton}
+                        ${nextButton}
+                    </div>
+                </div>
+            `);
         });
-        $('.body-container').html(`<div class="content-container">${dataBox.join('')}</div>
-        <div class="pagination"><button></button></div>
-        `);
-    });
 }
 
-showPage(swURL);
-
 $(document).ready(() => {
+    showPage(swURL);
     $('.search-input').keyup(() => {
         let word = $('.search-input').val();
-        if(word != '') {
+        if (word != '') {
             $('.body-container').html(`<div class="cannot-find"><img src="assets/images/loader.gif" alt=""></div>`);
             findMatch(word);
+        } else {
+            showPage(swURL);
         }
     });
+
 });
